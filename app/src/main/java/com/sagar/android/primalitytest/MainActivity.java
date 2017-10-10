103d0f62c5ef230f2b599bff76a5194c2c84fa58
sagar <snkumar.nayak@gmail.com>
2017-10-10 19:39:41 +0530

    view binding.

--- app/src/main/java/com/sagar/android/primalitytest/MainActivity.java
+++ app/src/main/java/com/sagar/android/primalitytest/MainActivity.java
@@ -3,18 +3,20 @@ package com.sagar.android.primalitytest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextNumber;
-    Button buttonCheck;
-    TextView textViewTimeTaken;
-    TextView textViewResult;
+    private EditText editTextNumber;
+    private Button buttonCheck;
+    private TextView textViewTimeTaken;
+    private TextView textViewResult;
 
-    enum Result {
+    private enum Result {
         PRIME("Prime"),
         NOT_PRIME("Not Prime");
 
@@ -37,5 +39,25 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         editTextNumber = (EditText) findViewById(R.id.edittext_number);
+        buttonCheck = (Button) findViewById(R.id.button_check);
+        textViewTimeTaken = (TextView) findViewById(R.id.textview_time_taken_value);
+        textViewResult = (TextView) findViewById(R.id.textview_result);
+
+        editTextNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
